622d70c02828228b74268ddc4e929a0b43677c93
animesh <animesh.sharma661199@gmail.com>
2024-10-10 19:16:11 +0530

    pylint fixes and added getSqlConnection.py to get sql connection

--- createTable.py
+++ createTable.py
@@ -1,23 +1,10 @@
-import pymysql
-from vars import *
-timeout = 10
-connection = pymysql.connect(
-  charset="utf8mb4",
-  connect_timeout=timeout,
-  cursorclass=pymysql.cursors.DictCursor,
-  db=DB,
-  host=SQLHOST,
-  password=SQLPASS,
-  read_timeout=timeout,
-  port=11252,
-  user=SQLUSER,
-  write_timeout=timeout,
-)
-
+from getSqlConnection import get_connection
+connection = get_connection()
 # run once to create the tabele in sql
 try:
     cursor = connection.cursor()
     cursor.execute("CREATE TABLE output_data (serial_no INT PRIMARY KEY, product_name VARCHAR(255) NOT NULL, input_image_urls TEXT NOT NULL, output_image_urls TEXT, status VARCHAR(25)) ")
+    # cursor.execute("select * from output_data")
     print(cursor.fetchall())
 finally:
   connection.close()
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import pandas as pd
+import uuid
 import os
+from io import BytesIO
+import pandas as pd
 import requests
 from PIL import Image
-from io import BytesIO
-import uuid
-import pymysql
+from getSqlConnection import get_connection
 # below vars.py contains following environment variables 
 # SQLHOST sql hostname
 # DB database name
@@ -16,20 +16,7 @@ import pymysql
 # CSV_OUTPUT path where outputcsv file will be stored
 from vars import *
 
-#sql connection details
-timeout = 10
-connection = pymysql.connect(
-  charset="utf8mb4",
-  connect_timeout=timeout,
-  cursorclass=pymysql.cursors.DictCursor,
-  db=DB,
-  host=SQLHOST,
-  password=SQLPASS,
-  read_timeout=timeout,
-  port=11252,
-  user=SQLUSER,
-  write_timeout=timeout,
-)
+connection = get_connection()
 
 # Directory to store processed images
 output_image_dir = OUTPUT_IMAGE_DIR
@@ -51,11 +38,9 @@ def process_image(image_url):
         # Download the image
         response = requests.get(image_url)
         img = Image.open(BytesIO(response.content))
-        
         # Compress image by 50%
         output_image_path = os.path.join(output_image_dir, f"{uuid.uuid4()}.jpg")
         img.save(output_image_path, format="JPEG", quality=50)  # Adjust quality for compression
-
         return output_image_path
     except Exception as e:
         print(f"Error processing image {image_url}: {e}")
@@ -64,36 +49,29 @@ def process_image(image_url):
 # Function to process the CSV
 def process_csv(input_csv, output_csv):
     df = pd.read_csv(input_csv)
-
     # Step 2: Validate CSV
     try:
         validate_csv(df)
     except ValueError as e:
         print(f"Validation error: {e}")
         return
-
     output_rows = []
-
-    # Step 3: Process each image URL
-    for _, row in df.iterrows():
-        
+    #Process each image URL
+    for _, row in df.iterrows(): 
         #Generate request ID for particular request
         request_id = generate_request_id()
         print(f"Request ID: {request_id}")
-
         try:
             cursor = connection.cursor()
             cursor.execute(f'''insert into {TABLE} (serial_no, product_name,input_image_urls,status, requestID)
                            VALUES 
                            (\'{row['S.No.']}\',\'{row['Product Name']}\',\'{row['Input Image Urls']}\','processing',\'{request_id}\')''')
-            print(cursor.fetchall())
+            # print(cursor.fetchall())
         except Exception as exception:
             print("Exception occured in SQL Connection",exception)
             exit()
-
         input_image_urls = row['Input Image Urls'].split(',')
         output_image_urls = []
-
         for image_url in input_image_urls:
             image_url = image_url.strip()
             processed_image_url = process_image(image_url)
@@ -101,21 +79,19 @@ def process_csv(input_csv, output_csv):
                 output_image_urls.append(processed_image_url)
             else:
                 output_image_urls.append('Error')
-
         output_rows.append({
             'S.No.': row['S.No.'],
             'Product Name': row['Product Name'],
             'Input Image Urls': row['Input Image Urls'],
             'Output Image Urls': ', '.join(output_image_urls)
         })
-
         try:
             cursor = connection.cursor()
             cursor.execute(f'''UPDATE {TABLE}
                                SET status = 'Processed',output_image_urls = \'{', '.join(output_image_urls)}\'       
                                WHERE serial_no = {output_rows[-1]['S.No.']}''')
             cursor.execute("commit")
-            print(cursor.fetchall())
+            # print(cursor.fetchall())
         except Exception as exception:
             print("Exception occured in SQL Connection",exception)
             exit()
@@ -124,13 +100,10 @@ def process_csv(input_csv, output_csv):
         print(cursor.fetchall())
     finally: 
         connection.close()
-
     #Save the processed data to a new CSV
     output_df = pd.DataFrame(output_rows)
     output_df.to_csv(output_csv, index=False)
     print(f"Processed data saved to {output_csv}")
-
-    
 
 # Assign a unique request ID
 def generate_request_id():
@@ -140,9 +113,5 @@ def generate_request_id():
 if __name__ == "__main__":
     input_csv = CSV_INPUT
     output_csv = CSV_OUTPUT
-    
     # Receive CSV and process images
-    
     process_csv(input_csv, output_csv)
-
-
